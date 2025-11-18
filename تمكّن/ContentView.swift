e1fcd45d08a7bf1ae8f87f8c792df32bdecdeddb
shahad <shada-123@hotmail.com>
2025-11-18 10:08:32 +0300

    Initial Commit

---
+++ تمكّن/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  تمكّن
+//
+//  Created by shahad khaled on 27/05/1447 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

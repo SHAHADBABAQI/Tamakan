1310e1814b824b8800ff6a83198cc94e2bab10f2
Nouran-salah-ali <nouransalahali@gmail.com>
2025-12-10 21:11:42 +0300

    Revert "count stuttring words in tirmnal done"
    This reverts commit 43631ca96ef37b878612dcd0fd44a2edf838029f.

--- تمكّن/Model/RecordingModel.swift
+++ تمكّن/Model/RecordingModel.swift
@@ -16,24 +16,22 @@ class RecordingModel: Identifiable {
     var date: Date
     var transcript: String
     var audiofile: URL
-    var countStuttringWords: Int
     //i have to intilise all data i can not init some of data
-    init(recordname: String, duration: Double, date: Date, transcript: String, audiofile: URL ,countStuttringWords :Int ) {
+    init(recordname: String, duration: Double, date: Date, transcript: String, audiofile: URL) {
         self.id = UUID()
         self.recordname = recordname
         self.duration = duration
         self.date = date
         self.transcript = transcript
         self.audiofile = audiofile
-        self.countStuttringWords = countStuttringWords
     }
     
     
 }//class
 
-struct StutterComment: Identifiable, Hashable {
-    let id = UUID()
-    let type: String        // e.g., "مد", "blocking", "repetition"
-    let word: String        // the word that was stuttered or blocked
-    let strategy: String    // optional strategy or note
+enum StutteringTypes: String , Codable{
+       case repetition
+       case prolongation
+       case block
 }
+
